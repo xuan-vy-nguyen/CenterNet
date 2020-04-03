fd19565bde57411bad1bf89507abcbc7b7422b93
Phạm Trọng Thắng <phamtrongthang123@gmail.com>
2020-04-03 21:08:23 +0700

    Update abc.py

--- src/lib/datasets/dataset/abc.py
+++ src/lib/datasets/dataset/abc.py
@@ -11,7 +11,7 @@ import os
 import torch.utils.data as data
 
 class COCO(data.Dataset):
-  num_classes = 80
+  num_classes = 3
   default_resolution = [512, 512]
   mean = np.array([0.40789654, 0.44719302, 0.47026115],
                    dtype=np.float32).reshape(1, 1, 3)
@@ -39,14 +39,7 @@ class COCO(data.Dataset):
     self.class_name = [
       'car','truck','other']
     self._valid_ids = [
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 
-      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 
-      24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 
-      37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 
-      48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
-      58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 
-      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 
-      82, 84, 85, 86, 87, 88, 89, 90]
+      1, 2, 3]
     self.cat_ids = {v: i for i, v in enumerate(self._valid_ids)}
     self.voc_color = [(v // 32 * 64 + 64, (v // 8) % 4 * 64, v % 8 * 32) \
                       for v in range(1, self.num_classes + 1)]
