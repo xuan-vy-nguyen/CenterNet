e1f684d84e15c4b563db93b6af2c04a588d396eb
xuan-vy-nguyen <xuanvy99@gmail.com>
2020-04-06 15:37:44 +0700

    config demo_2.py

--- src/demo_2.py
+++ src/demo_2.py
@@ -25,30 +25,19 @@ def demo(opt):
   Detector = detector_factory[opt.task]
   detector = Detector(opt)
 
-  if opt.demo == 'webcam' or \
-    opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
-    cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
-    detector.pause = False
-    while True:
-        _, img = cam.read()
-        cv2.imshow('input', img)
-        ret = detector.run(img)
-        time_str = ''
-        for stat in time_stats:
-          time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
-        print(time_str)
-        if cv2.waitKey(1) == 27:
-            return  # esc to quit
-  else:
-    if os.path.isdir(opt.demo):
+  ls_video = os.listdir(opt.demo)
+  for video in sorted(ls_video):
+    new_demo = os.path.join(opt.demo, video)
+    if os.path.isdir(new_demo):
       image_names = []
-      ls = os.listdir(opt.demo)
+      ls = os.listdir(new_demo)
       for file_name in sorted(ls):
           ext = file_name[file_name.rfind('.') + 1:].lower()
           if ext in image_ext:
-              image_names.append(os.path.join(opt.demo, file_name))
+              image_names.append(os.path.join(new_demo, file_name))
     else:
-      image_names = [opt.demo]
+      image_names = [new_demo]
+    
     detections = []
     for (image_name) in image_names:
       ret = detector.run(image_name)
@@ -75,7 +64,7 @@ def demo(opt):
               extreme_points = list(map(to_float, bbox[5:13]))
               detection["extreme_points"] = extreme_points
           detections.append(detection)
-    json.dump(detections,open('./results_demo.json', 'w'))
+    json.dump(detections,open('/home/leducthinh0409/json_frames_{}/{}.json'.format(opt.arch, new_demo.split('/')[-1]), 'w'))
 
 if __name__ == '__main__':
   opt = opts().init()
