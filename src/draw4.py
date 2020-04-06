6f910536e97d7d3b6abee38733de7e06b7e7d668
xuan-vy-nguyen <xuanvy99@gmail.com>
2020-04-06 17:46:25 +0700

    update draw4.py:

--- src/draw4.py
+++ src/draw4.py
@@ -4,14 +4,23 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import os
 
-count = 0
-lstDir= ['92', '95', '55', '71', '69', '82', '5', '51', '74', '68', '47', '10', '12', '38', '42', '46', '81', '84', '85', '54', '88', '67', '65', '8', '11', '98', '6', '3', '77', '97', '48', '43', '33', '14', '75', '50', '15', '62', '32', '4', '70', '36', '19', '34', '56', '53', '79', '73', '72', '39', '52', '9', '91', '29', '1', '22', '7', '66', '58', '44', '49', '24', '18', '78', '59', '13', '87', '40', '37', '86', '76', '17', '64', '41', '25', '30', '89', '83', '26', '21', '16', '90', '94', '57', '35', '60', '23', '28', '96', '99', '31', '100', '61', '93', '63', '20', '80', '27', '2', '45']
+name_model = 'dla_34'
+threshold = 0.3
 
-for lsd in lstDir:
+count = 0
+name_folder = 'json_'+ name_model
+video_folder = 'anno_videos_' + name_model
+ls_dir = os.listdir(name_folder)
+
+os.system('mkdir {}'.format(video_folder))
+
+# lsd = *.json
+for lsd in ls_dir:
 	# convert old format to new format
 	# {'image_name':[list of bbox]}
     dic_result_demo = {}
-    with open('/home/ptthang/aicity20/results_demo_json_4/{}_results_demo.json'.format(lsd),'r') as f:
+    print('open {}/{}.json'.format(name_folder, lsd))
+    with open('{}/{}'.format(name_folder, lsd),'r') as f:
         file_content = json.load(f)
         for ob in file_content:
             
@@ -24,19 +33,20 @@ for lsd in lstDir:
                 "bbox": ob["bbox"], 
                 "score": ob["score"]
             })
+
     # use new format to draw
-    os.system('mkdir /home/ptthang/aicity20/average_drawn_4_03/{}'.format(lsd))
+    os.system('mkdir {}/{}'.format(video_folder, lsd.split('.')[0]))
     for imgtmp, lsbbox in dic_result_demo.items():
-        img= '/home/ptthang/cli/average_image_test/{}/{}'.format(lsd,imgtmp.split('/')[-1])
-        print('drawing {}'.format(img))
+        img= 'extracted_frames/{}/{}'.format(lsd.split('.')[0], imgtmp.split('/')[-1])
+        print('+  drawing {}'.format(img))
         source_img = Image.open(img).convert("RGB")
         draw = ImageDraw.Draw(source_img)
         for ob in lsbbox:
-            if ob['score'] >= 0.3:
+            if ob['score'] >= threshold:
                 bbxs = ob['bbox']
                 draw.rectangle(((bbxs[0], bbxs[1]), (bbxs[0]+bbxs[2], bbxs[1]+bbxs[3])),outline='red', width=5)
-        source_img.save('/home/ptthang/aicity20/average_drawn_4_03/{}/{}'.format(lsd,img.split('/')[-1]))
-        print('saved /home/ptthang/aicity20/average_drawn_4_03/{}/{}'.format(lsd,img.split('/')[-1]))
+        source_img.save('{}/{}/{}'.format(video_folder,lsd.split('.')[0],img.split('/')[-1]))
+        print('saved {}/{}/{}'.format(video_folder,lsd.split('.')[0],img.split('/')[-1]))
         
 # print(count)
 # dic_result_demo
