53298320cd76df22d13f07c05cfec62613b0000a
xuan-vy-nguyen <xuanvy99@gmail.com>
2020-04-13 18:44:46 +0700

    remove cpu in bbox_out dataset/abc.py

--- src/lib/datasets/dataset/abc.py
+++ src/lib/datasets/dataset/abc.py
@@ -83,7 +83,7 @@ class COCO(data.Dataset):
               # "image_id": int(image_id),
               "image_id": image_id.cpu().detach(),
               "category_id": int(category_id),
-              "bbox": bbox_out.cpu().detach().numpy(),
+              "bbox": bbox_out,
               "score": float("{:.2f}".format(score.cpu().detach()))
           }
           if len(bbox) > 5:
